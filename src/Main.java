6578fe628573a16f94913939efbee5cf3cfe52df
Eric Abell <ericabell@users.noreply.github.com>
2017-09-13 10:56:04 -0500

    Fixed recursive call to exception handling to match assignment

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,58 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        // divideInts
         try {
             divideInts(2,3);
             divideInts(2,0);
-        } catch(Exception e) {
-            System.out.println("Divide by zero attempted!");
+        } catch(ArithmeticException e) {
+            System.out.println("ArithmeticException: " + e);
         }
 
+        // getSafeInt
+        getSafeInt();
+
+        // sumOfNumbers
+        try {
+            int[] myList = {1,2,3,4,5};
+            sumOfNumbers(myList, 5);
+        } catch(Exception e) {
+            System.out.println("Exception: " + e);
+        }
 
         System.out.println("\n****\nI'm done!!!\n****\n");
     }
 
-    public static int divideInts (int a, int b) {
-        return a/b;
+    public static int divideInts (int a, int b) throws ArithmeticException {
+        if( b==0 ) {
+            throw new ArithmeticException("Divide by zero");
+        } else {
+            return a/b;
+        }
     }
 
     public static int getSafeInt () {
-        return Integer.parseInt(new Scanner(System.in).nextLine());
-    }
-
-    public static int sumOfNumbers (int[] numbers, int length) {
-        int total = 0;
-        for (int num : numbers) {
-            total += num;
+        // NumberFormatException thrown by parseInt
+        // and for an empty string
+        int newInt = 0;
+        try {
+            newInt = Integer.parseInt(new Scanner(System.in).nextLine());
+        } catch (NumberFormatException e) {
+            getSafeInt();
         }
-        return total;
+
+        return newInt;
     }
 
+    public static int sumOfNumbers (int[] numbers, int length) throws Exception {
+        if( length != numbers.length ) {
+            throw new Exception("Lengths don't match!");
+        } else {
+            int total = 0;
+            for (int num : numbers) {
+                total += num;
+            }
+            return total;
+        }
+    }
 }
