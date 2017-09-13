e53ebdcf8dd6bb7e1fcb9ba5544858b9495b58e8
Eric Abell <ericabell@users.noreply.github.com>
2017-09-13 10:10:27 -0500

    Divide by zero exception handled

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        try {
+            divideInts(2,3);
+            divideInts(2,0);
+        } catch(Exception e) {
+            System.out.println("Divide by zero attempted!");
+        }
+
+
+        System.out.println("\n****\nI'm done!!!\n****\n");
     }
+
+    public static int divideInts (int a, int b) {
+        return a/b;
+    }
+
+    public static int getSafeInt () {
+        return Integer.parseInt(new Scanner(System.in).nextLine());
+    }
+
+    public static int sumOfNumbers (int[] numbers, int length) {
+        int total = 0;
+        for (int num : numbers) {
+            total += num;
+        }
+        return total;
+    }
+
 }
